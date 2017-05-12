f50a1d5950e3efb987130641a6fc4f9558f3825b
Sauvik Dolui <sauvikdolui@Sauviks-MacBook-Air.local>
2017-05-13 00:51:04 +0530

    Initial Commit

---
+++ SwiftLogger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftLogger
+//
+//  Created by Sauvik Dolui on 13/05/17.
+//  Copyright © 2017 Sauvik Dolui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
