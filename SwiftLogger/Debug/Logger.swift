c262c85d9c96e1a5767cc443a9b4c88de73f514e
saurabh <saurabh@esoftconnect.com>
2018-04-26 18:19:21 +0530

    More Flexibility is added to Logger class
    Changes in Logger File
    1.LogEvent enum is more informative.
    2.DateFormat include more type of date formats.
    3.dateFormatter stored property is now converted to computed property for being more flexible.
    4.log method name is changed to printDebugInfo with more default parameters.
    
    Changes in ViewController
    1.ViewController includes the usage of printDebugInfo function

--- SwiftLogger/Debug/Logger.swift
+++ SwiftLogger/Debug/Logger.swift
@@ -9,35 +9,75 @@
 import Foundation
 
 // Enum for showing the type of Log Types
-enum LogEvent: String {
-    case e = "[‼️]" // error
-    case i = "[ℹ️]" // info
-    case d = "[💬]" // debug
-    case v = "[🔬]" // verbose
-    case w = "[⚠️]" // warning
-    case s = "[🔥]" // severe
+enum LogEvent:String {
+    case error="Error Description"
+    case info="Info Messgae"
+    case debug="Debug Description"
+    case verbose="Verbose"
+    case warning="Warning Description"
+    case severe="Severe Description"
+}
+// Date formats to choose the desired format
+enum DateFormat:String
+{
+    case format_yyyyMMdd_hhmmssSSS = "yyyy-MM-dd hh:mm:ssSSS"
+    /*variables used for defining date structure in project*/
+    case  format_MMddyyyyHHmm = "MM-dd-yyyy HH:mm"
+    case  format_MMMddyyyyhhmmaaa = "MMM dd, yyyy hh:mm aaa"
+    case format_MMMddyyyyhhmma = "MMM dd, yyyy hh:mm a"
+    case format_HHmmss = "HH:mm:ss"
+    case format_hhmmaaa = "hh:mm aaa"
+    case format_hhmmaa = "hh:mm aa"
+    case format_yyyyMMdd = "yyyy-MM-dd"
+    case format_dd = "dd"
+    case format_EEEE = "EEEE"
+    case format_MMMM = "MMMM"
+    case format_yyyyMMddhhmmss = "yyyy-MM-dd HH:mm:ss"
+    case format_EEEEMMMddyyyy = "EEEE, MMM dd,yyyy"
+    case format_ddMMyyyy = "dd-MM-yyyy"
+    case format_MMMdd_yyyy = "MMM dd, yyyy"
+    case format_hhmma = "hh:mm a"
+    case format_MMMMdd = "MMMM dd"
+    case format_yyyy = "yyyy"
+    case format_d = "d"
+    case format_MM_dd_yyyy = "MM/dd/yyyy"
+    case format__d_ = " d "
+    case format_MMMddyyyy = "MMM dd,yyyy"
+    case format_MMddyyyy = "MM-dd-yyyy"
+    case format_ddyyyymm = "dd-yyyy-MM"
+    case format_yyyyMMddHHmm = "yyyy-MM-dd HH:mm"
+    
 }
 
 class Logger {
-    
-    static var dateFormat = "yyyy-MM-dd hh:mm:ssSSS"
-    static var dateFormatter: DateFormatter {
+    static var dateFormatter: (DateFormat)->DateFormatter = {
+        
         let formatter = DateFormatter()
-        formatter.dateFormat = dateFormat
+        formatter.dateFormat = $0.rawValue
         formatter.locale = Locale.current
         formatter.timeZone = TimeZone.current
         return formatter
     }
     
-    class func log(message: String,
-                   event: LogEvent,
-                   fileName: String = #file,
-                   line: Int = #line,
-                   column: Int = #column,
-                   funcName: String = #function) {
+    class func printDebugInfo(debugMessage: String="",
+                              event: LogEvent,
+                              withdateFormat:DateFormat=DateFormat.format_yyyyMMdd_hhmmssSSS,
+                              printData:Any?=nil,
+                              fileName: String = #file,
+                              line: Int = #line,
+                              column: Int = #column,
+                              funcName: String = #function) {
         
         #if DEBUG
-            print("\(Date().toString()) \(event.rawValue)[\(sourceFileName(filePath: fileName))]:\(line) \(column) \(funcName) -> \(message)")
+            print("\(event.rawValue):----\nDate :- \(Date().toString(withFormatter: withdateFormat))\nFunction Name :- \(funcName)\nLine number :- \(line)\nColumn Number :- \(column)\nFileName :- \(sourceFileName(filePath: fileName))\nFile Path :- \(fileName)")
+            if debugMessage.count > 0 || debugMessage != ""
+            {
+                print("Description :- \(debugMessage)\n")
+            }
+            if let _ = printData
+            {
+                print("Data--------\n\(printData!)")
+            }
         #endif
     }
     private class func sourceFileName(filePath: String) -> String {
@@ -45,9 +85,9 @@ class Logger {
         return components.isEmpty ? "" : components.last!
     }
 }
-
 internal extension Date {
-    func toString() -> String {
-        return Logger.dateFormatter.string(from: self as Date)
+    func toString(withFormatter formater:DateFormat) -> String {
+        return Logger.dateFormatter(formater).string(from: self as Date)
     }
 }
+
